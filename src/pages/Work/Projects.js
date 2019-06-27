f5f677cb8b5cdbec9c7693ac4bb8f7f676569d97
Sean Verhaagen <seanverhaagen@gmail.com>
2019-06-27 12:49:55 -0500

    added Doko to portfolio

--- src/pages/Work/Projects.js
+++ src/pages/Work/Projects.js
@@ -1,5 +1,12 @@
 const Projects = [
   {
+    name: "Doko",
+    image: '/portfolio-images/doko.png',
+    imageMin: '/portfolio-images/doko-min.png',
+    tags: ['React.js', 'Node.js', 'Socket.io'],
+    view: "http://welcome.dokomap.io",
+    message: "Doko is a personal project I have just released into beta, Its a worldwide chat application! You can place a chatroom anywhere in the world and begin talking in real time with other people."
+  },{
     name: "Workflow",
     image: '/portfolio-images/workflow.png',
     imageMin: '/portfolio-images/workflow-min.png',
